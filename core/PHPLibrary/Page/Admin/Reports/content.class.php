5970f7cf94da9db30938edcd434da325ef3632e0
Drelagas <drelagas.new@gmail.com>
2026-09-09 20:08:11 +0300

    0.3.1 Шуя - исправление мелких ошибок;

--- core/PHPLibrary/Page/Admin/Reports/content.class.php
+++ core/PHPLibrary/Page/Admin/Reports/content.class.php
@@ -28,7 +28,7 @@ use \core\PHPLibrary\Template as Template;
 use \core\PHPLibrary\Template\Collector as ThemeCollector;
 use \core\PHPLibrary\User as User;
 use \core\PHPLibrary\Entry as Entry;
-use \core\PHPLibrary\Page as PageStatic;
+use \core\PHPLibrary\PageStatic as PageStatic;
 
 /**
  * Class ReportsContent
